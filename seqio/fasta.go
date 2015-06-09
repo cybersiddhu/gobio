69183f786f93809d6b958e735ce83b44566c2a4d
Siddhartha Basu <biosidd@gmail.com>
2015-06-09 13:14:02 +0100

    Removed the regular expression based parsing of fasta header. Instead use byte slice to extract the header.

--- seqio/fasta.go
+++ seqio/fasta.go
@@ -1,7 +1,7 @@
 //Package seqio is a generic namespace shared by all biological sequence input and output
 //handlers.
-//This package contain a very barebone and simple Fasta format sequence file parser. 
-// Currently, it parses and returns the Id(header) and sequence. 
+//This package contain a very barebone and simple Fasta format sequence file parser.
+// Currently, it parses and returns the Id(header) and sequence.
 // This is mostly working concept, however could be easily extended in future.
 // Example
 //  package main
@@ -9,13 +9,14 @@
 //		"fmt"
 //		"github.com/cybersiddhu/gobio/seqio"
 //		"os"
+//		"log"
 //	)
 //
 //  func main() {
 //		 r := seqio.NewFastaReader(os.Args[1])
-//		 for r.HasEntry() {
-//				fasta := r.NextEentry()
-//				fmt.Printf("id:%s\nSequence:%s\n",f.Id,f.Sequence)
+//		 for r.HasEntry()  {
+//			fasta := r.NextEentry()
+//			fmt.Printf("id:%s\nSequence:%s\n",f.Id,f.Sequence)
 //		 }
 //  }
 package fasta
@@ -24,11 +25,9 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"log"
 	"os"
-	"regexp"
 )
-
-
 
 //A type for holding a single fasta record
 type Fasta struct {
@@ -38,48 +37,46 @@ type Fasta struct {
 
 //A data type for parsing one entry at a time
 type FastaReader struct {
-	reader      *bufio.Reader //pointer to a buffered reader
-//regular expression for parsing the header. For the time being, it will match any
-//non-whitespace character starting right after the ">" sign in the header.
-	fastaRegExp *regexp.Regexp 
-	seenHeader  bool
-	header      []byte
-	sequence    []byte
-	entry       *Fasta
-	exhausted   bool
+	reader *bufio.Reader //pointer to a buffered reader
+	//regular expression for parsing the header. For the time being, it will match any
+	//non-whitespace character starting right after the ">" sign in the header.
+	seenHeader bool
+	header     []byte
+	sequence   []byte
+	exhausted  bool
+	fasta      *Fasta
 }
 
-//Returns the next fasta entry 
+//Returns the next fasta entry
 func (f *FastaReader) NextEntry() *Fasta {
-	return f.entry
+	return f.fasta
 }
 
-
-//Checks for next fasta entry. Should be called before reading the next entry 
+//Checks for next fasta entry. Should be called before reading the next entry
 func (f *FastaReader) HasEntry() bool {
 	for {
-		line, err := f.reader.ReadSlice('\n')
-		if err == io.EOF {
-			if !f.exhausted {
-				f.exhausted = true
-				f.entry = &Fasta{
-					Id: f.header, Sequence: f.sequence,
-				}
-				return true
-			}
-			return false
-		}
-		if bytes.HasPrefix(line, []byte(">")) {
-			if match := f.fastaRegExp.FindSubmatch(line); match != nil {
-				if !f.seenHeader {
-					f.header = match[1]
-					f.seenHeader = true
-				} else {
-					f.entry = &Fasta{Id: f.header, Sequence: f.sequence}
-					f.header = match[1]
-					f.sequence = []byte{}
+		line, err := f.reader.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				if !f.exhausted {
+					f.exhausted = true
+					f.fasta = &Fasta{Id: f.header, Sequence: f.sequence}
 					return true
 				}
+				return false
+			} else {
+				log.Fatal(err)
+			}
+		}
+		if bytes.HasPrefix(line, []byte(">")) {
+			if !f.seenHeader {
+				f.header = line[1:len(line)]
+				f.seenHeader = true
+			} else {
+				f.fasta = &Fasta{Id: f.header, Sequence: f.sequence}
+				f.header = line[1:len(line)]
+				f.sequence = []byte{}
+				return true
 			}
 		} else {
 			f.sequence = append(f.sequence, bytes.TrimSuffix(line, []byte("\n"))...)
@@ -92,10 +89,9 @@ func (f *FastaReader) HasEntry() bool {
 func NewFastaReader(file string) *FastaReader {
 	reader, err := os.Open(file)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
 	return &FastaReader{
-		reader:      bufio.NewReader(reader),
-		fastaRegExp: regexp.MustCompile(`^>(\S+)`),
+		reader: bufio.NewReader(reader),
 	}
 }
